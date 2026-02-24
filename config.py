9abadb0c648a529a1dc13137c2bbf9c53824a2c1
hisi89131 <130222105+hisi89131@users.noreply.github.com>
2026-02-25 02:19:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./license.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://license_db_s13l_user:XJ9oJjODUQU8jP3qazXiMipVyvRxk7Qx@dpg-d6evsv15pdvs73f432cg-a.oregon-postgres.render.com/license_db_s13l)
 
 MAIN_ADMINS = ["1086634832"]
