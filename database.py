6e03b408bf710e287eacb091bc653bb515862f02
hisi89131 <130222105+hisi89131@users.noreply.github.com>
2026-02-25 03:00:37 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,9 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URL
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
